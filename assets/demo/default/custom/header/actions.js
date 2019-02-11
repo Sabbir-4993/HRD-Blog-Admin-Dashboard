52cdeb6d9d7d142755e19d608cb0398c7ba6c968
Sabbir15-4993 <sabbir15-4993@diu.edu.bd>
2019-02-11 11:16:49 +0600

    Initial commit

---
+++ assets/demo/default/custom/header/actions.js
@@ -0,0 +1,10 @@
+var ActionsDemo = {
+    init: function () {
+        $(".summernote").summernote({
+            height: 250
+        })
+    }
+};
+jQuery(document).ready(function () {
+    ActionsDemo.init()
+});
